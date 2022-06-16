8f214cdf89d13e2eab977548dd2cd5199965dffc
Anurag1020 <anurag.mendhe14@gmail.com>
2022-06-16 19:14:11 +0530

    uc2: emp wage cal

--- conditional_statements/Program.cs
+++ conditional_statements/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+const int DAILY_WAGE = 20, DAILY_HR = 8;
+Random random = new Random();
+int isEmppr = random.Next(0, 2);
+if(isEmppr == 1)
+{
+    Console.WriteLine(" present");
+    Console.WriteLine(DAILY_WAGE * DAILY_HR);
+}
+else
+{
+    Console.WriteLine(" abcent");
+}
